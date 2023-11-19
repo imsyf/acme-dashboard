ec6e62c7df4a7843ce59f56339913c4733599f4e
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-11-19 19:52:15 +0700

    Stream `<Card />` components

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,34 +1,26 @@
 import { Suspense } from "react";
 
-import { fetchCardData } from "@/app/lib/data";
 import { lusitana } from "@/app/ui/fonts";
 import {
+  CardsSkeleton,
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from "@/app/ui/skeletons";
 
-import { Card } from "../cards";
+import CardWrapper from "../cards";
 import LatestInvoices from "../latest-invoices";
 import RevenueChart from "../revenue-chart";
 
 export default async function Page() {
-  const {
-    customersCount,
-    invoicesCount,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={invoicesCount} type="invoices" />
-        <Card title="Total Customers" value={customersCount} type="customers" />
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -2,6 +2,17 @@
 const shimmer =
   "before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent";
 
+export function CardsSkeleton() {
+  return (
+    <>
+      <CardSkeleton />
+      <CardSkeleton />
+      <CardSkeleton />
+      <CardSkeleton />
+    </>
+  );
+}
+
 export function CardSkeleton() {
   return (
     <div
